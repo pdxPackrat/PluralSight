5ee72751b054736c847a8271ccac7f61b0910d73
Michael Christenson <mdc88@pdxchristenson.com>
2018-04-24 21:06:52 -0700

    part of module 5 exercise
    some equality overloading for module 5

--- equality_comparisons/equality/equality/CookedFood.cs
+++ equality_comparisons/equality/equality/CookedFood.cs
@@ -8,6 +8,33 @@ namespace equality.Classes
 {
     public sealed class CookedFood : Food   // sealed class, so nothing else can derive from it
     {
+        public static bool operator ==(CookedFood x, CookedFood y)
+        {
+            return object.Equals(x, y);
+        }
+
+        public static bool operator !=(CookedFood x, CookedFood y)
+        {
+            return !object.Equals(x, y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!base.Equals(obj)) // if the base class Equals method returns false then the instances are NOT equal
+                return false;
+
+            // note:  I trust that a null object can't be passed through because the base class Equals method checks for this already
+
+            // return true if base.Equals() returns true AND the derived type field is equal in this case
+            CookedFood rhs = (CookedFood)obj;
+            return this._cookingMethod == rhs._cookingMethod;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() ^ this._cookingMethod.GetHashCode();
+        }
+
         private string _cookingMethod;
 
         public string CookingMethod {  get { return _cookingMethod;  } }
--- equality_comparisons/equality/equality/Food.cs
+++ equality_comparisons/equality/equality/Food.cs
@@ -10,6 +10,35 @@ namespace equality.Classes
 
     public class Food
     {
+
+        public static bool operator ==(Food x, Food y)
+        {
+            return object.Equals(x, y);
+        }
+
+        public static bool operator !=(Food x, Food y)
+        {
+            return !object.Equals(x, y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (ReferenceEquals(obj, this)) // performance optimization
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+
+            Food rhs = obj as Food;  // force cast the object as a Food type at this point, having exhausted the other options
+            return this._name == rhs._name && this._group == rhs._group;  // now make sure that BOTH name and group are equal
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode() ^ this._group.GetHashCode();
+        }
+
         private readonly string _name;
         private readonly FoodGroup _group;
 
